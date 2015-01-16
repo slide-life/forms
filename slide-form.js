007f5a21ce6e03e88b282e10d942d8bafdaf2859
Matt Neary <neary.matt@gmail.com>
2015-01-16 17:50:31 -0500

    style updates and (de)serialization

--- slide-form.js
+++ slide-form.js
@@ -1,24 +1,24 @@
-var Slide = {
+var Forms = {
   listen: function() {
     $(".form li input").click(function(evt) {
       evt.preventDefault();
     });
     $(".form li").click(function(evt) {
       if( $(this).hasClass("active") ) {
-	$(this).removeClass("active");
+        $(this).removeClass("active");
       } else {
-	$(this).siblings().removeClass("active");
-	$(this).addClass("active");
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
       }
     });
     $(".form > li.select li").click(function(evt) {
       var $input = $(this).parents(".select").children("input");
       if( $(this).hasClass("other") ) {
-	$input.prop("readonly", false);
-	$input.focus();
+        $input.prop("readonly", false);
+        $input.focus();
       } else {
-	$input.val($(this).text());
-	$input.prop("readonly", true);
+        $input.val($(this).text());
+        $input.prop("readonly", true);
       }
     });
   },
@@ -28,34 +28,39 @@ var Slide = {
       className = "select";
     }
     var item = $("<li></li>", {
-      "class": className
+      "class": className,
+      "data-field": field.name
     });
     if( className == "text" ) {
       var input = $("<input>", {
-	"type": field.type,
-	"placeholder": field.name
+        "type": field.type,
+        "placeholder": "...",
+        "autocorrect": "off",
+        "autocapitalize": "off"
       });
       item.append(input);
     } else if( className == "select" ) {
       var input = $("<input>", {
-	"type": field.subType,
-	"placeholder": field.name,
-	"readonly": true
+        "type": field.subType,
+        "value": field.options[0],
+        "readonly": true,
+        "autocorrect": "off",
+        "autocapitalize": "off"
       });
       var options = $("<ul>", {
         "class": "options"
       });
       field.options.forEach(function(option) {
-	var optionElm = $("<li>");
-	optionElm.text(option);
+        var optionElm = $("<li>");
+        optionElm.text(option);
         options.append(optionElm);
       });
       if( field.custom ) {
-	var otherElm = $("<li>", {
-	  "class": "other"
-	});
-	otherElm.text("Other");
-	options.append(otherElm);
+        var otherElm = $("<li>", {
+          "class": "other"
+        });
+        otherElm.text("Other");
+        options.append(otherElm);
       }
       item.append(input);
       item.append(options);
@@ -68,27 +73,35 @@ var Slide = {
     });
   },
   textField: function(name) {
-    return Slide.createField({ type: "text", name: name });
+    return Forms.createField({ type: "text", name: name });
   },
   numberField: function(name) {
-    return Slide.createField({ type: "number", name: name });
+    return Forms.createField({ type: "number", name: name });
   },
   selectField: function(name, options, custom) {
-    return Slide.createField({
+    return Forms.createField({
       type: "select",
       subType: "text",
       name: name,
       options: options,
       custom: custom
     });
+  },
+  populateForm: function(fields) {
+    $(function() {
+      Forms.createForm($(".form"), fields);
+      Forms.listen();
+    });
+  },
+  serializeForm: function() {
+    var fields = $(".form").children("li");
+    var keystore = {};
+    [].map.call(fields, function(field) {
+      var key = $(field).data("field"),
+          value = $(field).find("input").val();
+      keystore[key] = value;
+    });
+    return JSON.stringify(keystore);
   }
 };
-$(function() {
-  Slide.createForm($(".form"), [
-    Slide.textField("First Name"),
-    Slide.textField("Last Name"),
-    Slide.numberField("Card Number"),
-    Slide.selectField("Sex", ["Male", "Female"], true)]);
-  Slide.listen();
-});
 
